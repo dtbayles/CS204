bc498627d0ea36bccaa15dd8006582bfe413082e
Drew Bayles <dtbayles@gmail.com>
2021-11-06 02:18:56 -0600

    Added aspectJ lab

---
+++ aspectJ_lab/src/main/java/YourClass.java
@@ -0,0 +1,12 @@
+public class YourClass {
+
+    public static void main(String[] args) {
+        YourClass yourClass = new YourClass();
+        yourClass.yourMethodAround();
+    }
+
+    @YourAnnotation
+    public void yourMethodAround(){
+        System.out.println("Executing TestTarget.yourMethodAround()");
+    }
+}
